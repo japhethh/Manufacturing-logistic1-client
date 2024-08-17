d2f777c3dcd8c4f85cea4908d8d9b7c4ecdf44ce
AdieDev0 <devutella@gmail.com>
2024-08-17 23:02:10 +0800

    Register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './Components/Nav'
+import Register from './Components/Register'
 
 const App = () => {
   return (
     <div>
       <Nav/>
+      <Register/>
     </div>
   )
 }
