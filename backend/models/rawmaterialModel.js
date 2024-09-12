8f0aec10625fc181ead0dbbe66417229b7c100b3
japhethh <boknoy32130@gmail.com>
2024-09-13 02:44:27 +0800

    Created Rawmaterial Model

---
+++ backend/models/rawmaterialModel.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const rawmaterialSchema = mongoose.Schema({
+  requestData:{
+    type:Date,
+    default:Date.now
+  },
+  requestStatus:{
+    type:String,
+    enum:['Pending','Approved','Rejected'], 
+    default:'Pending'
+  },
+  requestedBy:{
+    type:String,
+    required:true
+  },
+  material:[{
+    materialName:String,
+    materialId:{type:mongoose.Schema.Types.ObjectId, ref:"Material"},
+    quantity:Number,
+    unit:String,
+  }
+],
+  priority:{
+    type:String,
+    enum:['High','Medium','Low'],
+    default:'Medium'
+  },
+  financeApproval:{
+    type:Boolean,
+    default:false
+  }
+});
+
+  const rawmaterialModel = mongoose.model('RawmaterialRequest',rawmaterialSchema);
+
+
+  export default rawmaterialModel;
