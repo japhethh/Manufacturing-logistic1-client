8eaf20eb5a4c64c9a58a16ce172401d869bc6e33
AdieDev0 <devutella@gmail.com>
2024-09-14 00:07:01 +0800

    headwind

--- frontend/src/Components/Orderhistory.jsx
+++ frontend/src/Components/Orderhistory.jsx
@@ -26,45 +26,45 @@ const Orderhistory = () => {
 
   return (
     <div className="p-4">
-      <div className="w-full mb-2 flex justify-end">
-        <button className="py-2 px-3 rounded-full border border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white duration-150">
+      <div className="flex justify-end w-full mb-2">
+        <button className="px-3 py-2 text-blue-700 duration-150 border border-blue-700 rounded-full hover:bg-blue-700 hover:text-white">
           Create Request
         </button>
       </div>
 
       <div className="overflow-x-auto">
         {loading ? (
-          <div className="text-center py-4">Loading...</div>
+          <div className="py-4 text-center">Loading...</div>
         ) : error ? (
-          <div className="text-center py-4 text-red-500">{error}</div>
+          <div className="py-4 text-center text-red-500">{error}</div>
         ) : (
-          <table className="table table-xs w-full border-collapse">
+          <table className="table w-full border-collapse table-xs">
             {/* head */}
             <thead>
               <tr className="bg-stone-200 text-black/70">
-                <th className="border px-4 py-2"></th>
-                <th className="border px-4 py-2">Request By</th>
-                <th className="border px-4 py-2">Material</th>
-                <th className="border px-4 py-2">Request Data</th>
-                <th className="border px-4 py-2">Priority</th>
-                <th className="border px-4 py-2">Request Status</th>
+                <th className="px-4 py-2 border"></th>
+                <th className="px-4 py-2 border">Request By</th>
+                <th className="px-4 py-2 border">Material</th>
+                <th className="px-4 py-2 border">Request Data</th>
+                <th className="px-4 py-2 border">Priority</th>
+                <th className="px-4 py-2 border">Request Status</th>
               </tr>
             </thead>
             <tbody>
               {users.length > 0 ? (
                 users.map((user, index) => (
                   <tr key={index} className="bg-base-100">
-                    <td className="border px-4 py-2">{index + 1}</td>
-                    <td className="border px-4 py-2">{user.requestedBy}</td>
-                    <td className="border px-4 py-2">{user.material}</td>
-                    <td className="border px-4 py-2">{user.requestedData}</td>
-                    <td className="border px-4 py-2">{user.priority}</td>
-                    <td className="border px-4 py-2">{user.requestedStatus}</td>
+                    <td className="px-4 py-2 border">{index + 1}</td>
+                    <td className="px-4 py-2 border">{user.requestedBy}</td>
+                    <td className="px-4 py-2 border">{user.material}</td>
+                    <td className="px-4 py-2 border">{user.requestedData}</td>
+                    <td className="px-4 py-2 border">{user.priority}</td>
+                    <td className="px-4 py-2 border">{user.requestedStatus}</td>
                   </tr>
                 ))
               ) : (
                 <tr>
-                  <td colSpan="4" className="text-center border px-4 py-2">
+                  <td colSpan="4" className="px-4 py-2 text-center border">
                     No users found
                   </td>
                 </tr>
