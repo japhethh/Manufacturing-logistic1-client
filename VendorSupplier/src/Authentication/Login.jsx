cc85a841a9786dbcc36ed7bf042b3e5a5c804ba6
AdieDev0 <devutella@gmail.com>
2024-09-14 05:46:27 +0800

    VendorSup Register (UNDERCONS)

---
+++ VendorSupplier/src/Authentication/Login.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("hello");
+  };
+
+  return (
+    // component
+    <div className="flex items-center justify-center w-full min-h-screen bg-gray-50">
+      <div className="max-w-md px-8 py-6 bg-white rounded-lg shadow-md">
+        <h1 className="mb-4 text-2xl font-bold text-center text-gray-900">
+          Welcome Back!
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-700"
+            >
+              Email Address
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              placeholder="your@email.com"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              placeholder="Enter your password"
+              required
+            />
+            <a
+              href="#"
+              className="text-xs text-gray-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Forgot Password?
+            </a>
+          </div>
+          <div className="flex items-center justify-between mb-4">
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                id="remember"
+                className="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500 focus:outline-none"
+                defaultChecked
+              />
+              <label
+                htmlFor="remember"
+                className="block ml-2 text-sm text-gray-700"
+              >
+                Remember me
+              </label>
+            </div>
+            <Link to="/register">
+              <a
+                href="#"
+                className="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Create Account
+              </a>
+            </Link>
+          </div>
+          <button
+            type="submit"
+            className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
