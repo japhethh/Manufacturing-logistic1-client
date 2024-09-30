374c7167fa83b93e101cd9c64ff08748285a266c
japhethh <boknoy32130@gmail.com>
2024-09-30 09:04:50 +0800

    Fixing the MaterialItem if they add new Item in the supplier it will be working

--- backend/controllers/materialController.js
+++ backend/controllers/materialController.js
@@ -1,6 +1,7 @@
 import asyncHandler from "express-async-handler";
 
 import MaterialModel from "../models/materialModel.js";
+import supplierModel from "../models/supplierModel.js";
 // Create
 const createMaterial = asyncHandler(async (req, res) => {
   const {
@@ -25,7 +26,7 @@ const createMaterial = asyncHandler(async (req, res) => {
     return res.status(400).json("Enter all field!");
   }
 
-  const material =  new MaterialModel({
+  const material = new MaterialModel({
     materialName: materialName,
     materialCode: materialCode,
     description: description,
@@ -41,9 +42,49 @@ const createMaterial = asyncHandler(async (req, res) => {
 
 // Get all material
 const getAllMaterial = asyncHandler(async (req, res) => {
-  const materials = await MaterialModel.find({}).populate("supplier").sort({ orderDate: -1 });;
+  const materials = await MaterialModel.find({})
+    .populate("supplier")
+    .sort({ orderDate: -1 });
   res.status(200).json(materials);
 });
 // Get a single material ID
 
-export { createMaterial, getAllMaterial };
+const appendMaterial = asyncHandler(async (req, res) => {
+  const {
+    materialName,
+    materialCode,
+    description,
+    unit,
+    pricePerUnit,
+    available,
+  } = req.body;
+  const { id } = req.params;
+
+  const supplierUser = await supplierModel.findById(id);
+
+  if (!supplierUser) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Supplier Not Found" });
+  }
+
+  const newMaterial = new MaterialModel({
+    materialName: materialName,
+    materialCode: materialCode,
+    description: description,
+    unit: unit,
+    pricePerUnit: pricePerUnit,
+    supplier: id,
+    available: available,
+  });
+
+  const savedMaterial = await newMaterial.save();
+
+  supplierUser.materialSupplied.push(savedMaterial._id);
+
+  await supplierUser.save();
+
+  res.status(200).json({ success: true, data: supplierUser });
+});
+
+export { createMaterial, getAllMaterial, appendMaterial };
--- backend/routes/materialRouter.js
+++ backend/routes/materialRouter.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { getAllMaterial,createMaterial } from '../controllers/materialController.js';
+import { getAllMaterial,createMaterial,appendMaterial } from '../controllers/materialController.js';
+import { authMiddleware } from '../middleware/Auth.js';
 
 const materialRouter = express.Router();
 
 
 materialRouter.get("/getAllMaterial",getAllMaterial);
 materialRouter.post("/addMaterial",createMaterial)
+materialRouter.put("/appendMaterial/:id",appendMaterial)
 
 
 export default materialRouter;
