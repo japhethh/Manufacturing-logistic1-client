31dbb81f884fd40d3bf124b0a25f3ee45a7aad01
japhethh <boknoy32130@gmail.com>
2024-10-13 01:45:00 +0800

    "Updated import path for VendorUserContextProvider from './context/vendorUserContext.jsx' to './context/VendorUserContext.jsx'"

--- VendorSupplier/src/main.jsx
+++ VendorSupplier/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import VendorUserContextProvider from "./context/vendorUserContext.jsx";
+import VendorUserContextProvider from "./context/VendorUserContext.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <VendorUserContextProvider>
