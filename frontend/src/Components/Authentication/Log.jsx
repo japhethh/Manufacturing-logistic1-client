2d4c90ad5a4681d2a52650b86fef1fe778f6be52
AdieDev0 <devutella@gmail.com>
2024-10-29 14:07:22 +0800

    Blue Login (Logistic 1)

--- frontend/src/Components/Authentication/Log.jsx
+++ frontend/src/Components/Authentication/Log.jsx
@@ -63,7 +63,7 @@ const Log = () => {
             <input
               type="text"
               id="userName"
-              className="shadow-sm rounded-md w-full px-4 py-2 border border-gray-300 focus:outline-none focus:ring-green-500 focus:border-green-500"
+              className="shadow-sm rounded-md w-full px-4 py-2 border border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
               placeholder="Enter your username..."
               {...register("userName", { required: "Username is required" })}
             />
@@ -78,14 +78,14 @@ const Log = () => {
             <input
               type="password"
               id="password"
-              className="shadow-sm rounded-md w-full px-4 py-2 border border-gray-300 focus:outline-none focus:ring-green-500 focus:border-green-500"
+              className="shadow-sm rounded-md w-full px-4 py-2 border border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
               placeholder="Enter your password"
               {...register("password", { required: "Password is required" })}
             />
             {errors.password && (
               <span className="text-red-500">{errors.password.message}</span>
             )}
-            <a href="#" className="text-xs text-gray-600 hover:text-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+            <a href="#" className="text-xs text-gray-600 hover:text-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
               Forgot Password?
             </a>
           </div>
@@ -94,7 +94,7 @@ const Log = () => {
               <input
                 type="checkbox"
                 id="remember"
-                className="h-4 w-4 rounded border-gray-300 text-green-600 focus:ring-green-500 focus:outline-none"
+                className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500 focus:outline-none"
                 defaultChecked
               />
               <label htmlFor="remember" className="ml-2 block text-sm text-gray-700">
@@ -104,7 +104,7 @@ const Log = () => {
           </div>
           <button
             type="submit"
-            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
           >
             Login
           </button>
