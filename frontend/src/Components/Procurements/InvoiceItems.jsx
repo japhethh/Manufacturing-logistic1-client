2b11e447e1daa890b87ac2d84488c7a52e209fb5
AdieDev0 <devutella@gmail.com>
2025-01-18 14:30:50 +0800

    Enhance InvoiceItems Cards

--- frontend/src/Components/Procurements/InvoiceItems.jsx
+++ frontend/src/Components/Procurements/InvoiceItems.jsx
@@ -57,7 +57,7 @@ const InvoiceItems = () => {
   }
 
   return (
-    <div className="px-6 pt-6  bg-gradient-to-b from-gray-100 to-gray-50 relative w-full">
+    <div className="px-6 pt-6  relative w-full">
       {/* Loading Spinner */}
       {loading && (
         <div className="absolute inset-0 flex justify-center items-center bg-white bg-opacity-75 z-50">
@@ -67,53 +67,60 @@ const InvoiceItems = () => {
 
       {/* CARDS SELECTION */}
       <div
-  className={`grid grid-cols-1 md:grid-cols-3 gap-6 mb-10 transition-opacity duration-500 ${
-    loading ? "opacity-0" : "opacity-100"
-  }`}
->
-  <NavLink to="/invoice/all" className="w-full" aria-label="All Invoice">
-    <div className="card bg-white shadow-xl rounded-lg p-6 text-center transition-all transform hover:scale-105 hover:shadow-2xl hover:cursor-pointer">
-      <div className="flex items-center justify-between mb-6">
-        <h2 className="text-gray-800 font-semibold text-lg sm:text-xl">
-          All Invoice
-        </h2>
-        <AiOutlineForm className="text-4xl sm:text-5xl text-blue-500 transition-transform transform hover:scale-110" />
-      </div>
-      <span className="badge bg-gradient-to-r from-blue-300 to-blue-500 text-white rounded-full px-3 py-1 text-xs font-semibold">
-        {allResult}
-      </span>
-    </div>
-  </NavLink>
+        className={`grid grid-cols-1 md:grid-cols-3 gap-6 mb-10 transition-opacity duration-500 ${
+          loading ? "opacity-0" : "opacity-100"
+        }`}
+      >
+        <NavLink to="/invoice/all" className="w-full" aria-label="All Invoice">
+          <div className="card bg-gradient-to-br from-blue-100 to blue-50 shadow-xl rounded-lg p-6 text-center transition-all transform hover:scale-105 hover:shadow-2xl hover:cursor-pointer">
+            <div className="flex items-center justify-between mb-6">
+              <h2 className="text-blue-800 font-bold text-lg sm:text-xl">
+                All Invoice
+              </h2>
+              <AiOutlineForm className="text-4xl sm:text-5xl text-blue-500 transition-transform transform hover:scale-110" />
+            </div>
+            <span className="badge bg-gradient-to-r from-blue-300 to-blue-500 text-white rounded-full px-3 py-1 text-xs font-semibold">
+              {allResult}
+            </span>
+          </div>
+        </NavLink>
 
-  <NavLink to="/invoice/pending" className="w-full" aria-label="Pending Invoice">
-    <div className="card bg-white shadow-xl rounded-lg p-6 text-center transition-all transform hover:scale-105 hover:shadow-2xl hover:cursor-pointer">
-      <div className="flex items-center justify-between mb-6">
-        <h2 className="text-gray-800 font-semibold text-lg sm:text-xl">
-          Pending Invoice
-        </h2>
-        <AiOutlineForm className="text-4xl sm:text-5xl text-blue-500 transition-transform transform hover:scale-110" />
-      </div>
-      <span className="badge bg-gradient-to-r from-blue-300 to-blue-500 text-white rounded-full px-3 py-1 text-xs font-semibold">
-        {pendingResult}
-      </span>
-    </div>
-  </NavLink>
+        <NavLink
+          to="/invoice/pending"
+          className="w-full"
+          aria-label="Pending Invoice"
+        >
+          <div className="card bg-gradient-to-br from-teal-100 to-teal-50 shadow-xl rounded-lg p-6 text-center transition-all transform hover:scale-105 hover:shadow-2xl hover:cursor-pointer">
+            <div className="flex items-center justify-between mb-6">
+              <h2 className="text-teal-800 font-bold text-lg sm:text-xl">
+                Pending Invoice
+              </h2>
+              <AiOutlineForm className="text-4xl sm:text-5xl text-teal-500 transition-transform transform hover:scale-110" />
+            </div>
+            <span className="badge bg-gradient-to-r from-teal-300 to-teal-500 text-white rounded-full px-3 py-1 text-xs font-semibold">
+              {pendingResult}
+            </span>
+          </div>
+        </NavLink>
 
-  <NavLink to="/invoice/complete" className="w-full" aria-label="Complete Invoice">
-    <div className="card bg-white shadow-xl rounded-lg p-6 text-center transition-all transform hover:scale-105 hover:shadow-2xl hover:cursor-pointer">
-      <div className="flex items-center justify-between mb-6">
-        <h2 className="text-gray-800 font-semibold text-lg sm:text-xl">
-          Complete Invoice
-        </h2>
-        <FiPackage className="text-4xl sm:text-5xl text-green-500 transition-transform transform hover:scale-110" />
+        <NavLink
+          to="/invoice/complete"
+          className="w-full"
+          aria-label="Complete Invoice"
+        >
+          <div className="card bg-gradient-to-br from-green-100 to-green-50 shadow-xl rounded-lg p-6 text-center transition-all transform hover:scale-105 hover:shadow-2xl hover:cursor-pointer">
+            <div className="flex items-center justify-between mb-6">
+              <h2 className="text-green-800 font-bold text-lg sm:text-xl">
+                Complete Invoice
+              </h2>
+              <FiPackage className="text-4xl sm:text-5xl text-green-500 transition-transform transform hover:scale-110" />
+            </div>
+            <span className="badge bg-gradient-to-r from-green-300 to-green-500 text-white rounded-full px-3 py-1 text-xs font-semibold">
+              {approveResult}
+            </span>
+          </div>
+        </NavLink>
       </div>
-      <span className="badge bg-gradient-to-r from-green-300 to-green-500 text-white rounded-full px-3 py-1 text-xs font-semibold">
-        {approveResult}
-      </span>
-    </div>
-  </NavLink>
-</div>
-
 
       {/* Content outlet */}
       <div
