2d561897942508fc3e85e1c7bad0986551071352
AdieDev0 <devutella@gmail.com>
2025-01-23 11:00:35 +0800

    Sidebar Invoice

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -524,20 +524,32 @@ const Sidebar = () => {
             </li>
 
             {/* INVOICE SECTION */}
-            <NavLink to="/invoice/all">
-              <li className={`menu-item ${isCollapsed ? "hidden" : ""}`}>
-                <div className="flex mt-1 items-center cursor-pointer">
-                  <FaFileInvoice
-                    className={`transition-all duration-300 ${
-                      isCollapsed ? "w-7 h-7" : "w-5 h-5"
-                    }`}
-                  />
-                  {!isCollapsed && (
-                    <span className="text-base font-semibold">Invoice</span>
-                  )}
-                </div>
-              </li>
-            </NavLink>
+            <li
+              className={`menu-item transition-all duration-300 ${
+                isCollapsed ? "hidden" : ""
+              }`}
+            >
+              <NavLink
+                to="/invoice/all"
+                className={({ isActive }) =>
+                  `flex items-center gap-2 mt-1  rounded-md transition-colors duration-200 ease-in-out 
+        ${
+          isActive
+            ? "text-base-200 bg-blue-500 font-bold"
+            : "text-black hover:bg-gray-100"
+        }`
+                }
+              >
+                <FaFileInvoice
+                  className={`transition-all duration-300 ${
+                    isCollapsed ? "w-7 h-7" : "w-5 h-5"
+                  }`}
+                />
+                {!isCollapsed && (
+                  <span className="text-base font-semibold">Invoice</span>
+                )}
+              </NavLink>
+            </li>
             {/* PAYMENT SECTION */}
             <NavLink to="/paymentList">
               <li className={`menu-item ${isCollapsed ? "hidden" : ""}`}>
