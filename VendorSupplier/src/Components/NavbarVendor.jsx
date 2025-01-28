95d863ff347ebb1189317baf451fee3db764ed01
AdieDev0 <devutella@gmail.com>
2025-01-28 11:16:50 +0800

    Update Notification

--- VendorSupplier/src/Components/NavbarVendor.jsx
+++ VendorSupplier/src/Components/NavbarVendor.jsx
@@ -231,7 +231,7 @@ const NavbarVendor = () => {
                     d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                   />
                 </svg>
-                <span className="badge badge-xs badge-primary indicator-item">
+                <span className="badge badge-sm bg-gradient-to-tr from-blue-400 to-blue-700 indicator-item font-semibold text-white">
                   {notifications.filter((notif) => !notif.isRead).length}
                 </span>
               </div>
